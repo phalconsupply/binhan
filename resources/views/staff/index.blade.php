732934255bca50586cc9a33e536455587e9a3561
Phalcon <dy01hoaquason@gmail.com>
2025-11-22 22:06:17 +0700

    Add earnings button for drivers, medical staff and managers in staff list

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -126,6 +126,9 @@
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-2">
                                             <a href="{{ route('staff.show', $member) }}" class="text-blue-600 hover:text-blue-900">Xem</a>
+                                            @if(in_array($member->staff_type, ['driver', 'medical_staff', 'manager']))
+                                            <a href="{{ route('staff.earnings', $member) }}" class="text-green-600 hover:text-green-900">Thu nhập</a>
+                                            @endif
                                             @can('edit staff')
                                             <a href="{{ route('staff.edit', $member) }}" class="text-indigo-600 hover:text-indigo-900">Sửa</a>
                                             @endcan
