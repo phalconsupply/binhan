fa1068177fbe63d9b865148b05f0d4be83498a5f
Phalcon <dy01hoaquason@gmail.com>
2025-11-22 18:45:43 +0700

    Add missing DB facade import in TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,6 +6,7 @@ use App\Models\Transaction;
 use App\Models\Vehicle;
 use App\Models\Incident;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
