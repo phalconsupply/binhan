967be11a141335796f6d7d66be190d015d778419
Phalcon <dy01hoaquason@gmail.com>
2025-12-25 11:02:06 +0700

    Fix layouts/app: support cả component slot và blade yield syntax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,11 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @isset($slot)
+                    {{ $slot }}
+                @else
+                    @yield('content')
+                @endisset
             </main>
         </div>
 
